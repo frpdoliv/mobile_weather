43150549d315ce1f596b64ab8990c90d5794f446
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-14 23:47:34 +0100

    Repalced Text widget with RichText to be able to do fine-grain styling

--- lib/widgets/weather_card.dart
+++ lib/widgets/weather_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_weather/model/weather_data.dart';
 
 class WeatherCard extends StatelessWidget {
@@ -6,6 +7,21 @@ class WeatherCard extends StatelessWidget {
 
   final WeatherData weatherData;
   final Color? _dividerColor = Colors.grey[350]; 
+  final TextStyle _weatherStateStyle = GoogleFonts.ubuntu(
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontSize: 24.0,
+  );
+  final TextStyle _valueDescriptionStyle = GoogleFonts.ubuntu(
+    color: Colors.grey[850],
+    fontWeight: FontWeight.w600,
+    fontSize: 14.0,
+  ); 
+  final TextStyle _valueStyle = GoogleFonts.ubuntu(
+    color: Colors.grey,
+    fontSize: 14.0,
+  ); 
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +40,14 @@ class WeatherCard extends StatelessWidget {
           ),
           child: Column (
             children: <Widget> [
-              const Expanded (
-                flex: 1,
+              Expanded (
+                flex: 2,
                 child: Align(
                   alignment: Alignment.center,
-                  child: Text('Ceu Mt Nubladuh'),
+                  child: Text(
+                    'Ceu Mt Nubladuh',
+                    style: _weatherStateStyle,
+                  ),
                 ),
               ),
               Divider (
@@ -37,7 +56,7 @@ class WeatherCard extends StatelessWidget {
                 endIndent: 0.5,
               ),
               Expanded (
-                flex: 7,
+                flex: 6,
                 child: Align(
                   alignment: Alignment.center,
                   child: Image.asset('assets/weather_indicators/${weatherData.imgCode}.png')
@@ -54,12 +73,38 @@ class WeatherCard extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget> [
-                    Text('MIN: ${weatherData.minTemp}'),
+                    RichText(
+                      text: TextSpan (
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'MIN: ',
+                            style: _valueDescriptionStyle,
+                          ),
+                          TextSpan(
+                            text: '${weatherData.minTemp}',
+                            style: _valueStyle,
+                          ),
+                        ]
+                      ),
+                    ),
                     VerticalDivider(
                       color: _dividerColor,
                       width: 50.0,
                     ),
-                    Text('MAX: ${weatherData.maxTemp}'),
+                    RichText(
+                      text: TextSpan (
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'MAX: ',
+                            style: _valueDescriptionStyle,
+                          ),
+                          TextSpan(
+                            text: '${weatherData.maxTemp}',
+                            style: _valueStyle,
+                          ),
+                        ]
+                      ),
+                    ),
                   ],
                 ),
               )
