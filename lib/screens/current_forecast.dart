ce20a07797aa107398b55b9b0c19843488fb0994
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-18 23:07:25 +0100

    Created three widget to represent the three screens

---
+++ lib/screens/current_forecast.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CurrentForecast extends StatelessWidget {
+  const CurrentForecast({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/screens/hourly_forecast.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HourlyForecast extends StatelessWidget {
+  const HourlyForecast({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
