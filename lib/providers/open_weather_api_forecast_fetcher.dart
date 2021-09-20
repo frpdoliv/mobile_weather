86fdade4a0276e30397179e90329a47d7b9728e0
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-20 11:38:08 +0100

    Created the class that will fetch data from open weather api

---
+++ lib/providers/open_weather_api_forecast_fetcher.dart
@@ -0,0 +1,11 @@
+import 'package:mobile_weather/providers/forecast_fetcher.dart';
+
+class OpenWeatherAPIForecastFetcher extends ForecastFetcher {
+  static const String apiKey = //TODO Insert open weather API key here
+  
+  @override
+  Future<Map<String, dynamic>> fetch() {
+    // TODO: implement fetch
+    throw UnimplementedError();
+  }
+}
