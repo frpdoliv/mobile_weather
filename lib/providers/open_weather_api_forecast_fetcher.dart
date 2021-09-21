87917601074be174d2d07f8d999a07a67c7fb6b0
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-21 21:08:01 +0100

    Implemented fetch method

--- lib/providers/open_weather_api_forecast_fetcher.dart
+++ lib/providers/open_weather_api_forecast_fetcher.dart
@@ -1,11 +1,22 @@
+import 'dart:convert';
 import 'package:mobile_weather/providers/forecast_fetcher.dart';
+import 'package:http/http.dart' as http;
 
 class OpenWeatherAPIForecastFetcher extends ForecastFetcher {
-  static const String apiKey = //TODO Insert open weather API key here
+  final String latitude;
+  final String longitude;
   
+  OpenWeatherAPIForecastFetcher._({required this.latitude, required this.longitude});
+
+  static Future<OpenWeatherAPIForecastFetcher> create({required String longitude, required String latitude}) async {
+    OpenWeatherAPIForecastFetcher newInstance = OpenWeatherAPIForecastFetcher._(longitude: longitude, latitude: latitude);
+    await newInstance.update();
+    return newInstance;
+  } 
+
   @override
-  Future<Map<String, dynamic>> fetch() {
-    // TODO: implement fetch
-    throw UnimplementedError();
+  Future<Map<String, dynamic>> fetch() async {
+    http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/onecall?lat=$latitude&lon=$longitude&appid=$apiKey'));
+    return jsonDecode(response.body);
   }
 }
