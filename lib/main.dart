78e75b8dcc6d29dad56918e52c404ee10da40ce9
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-26 23:55:42 +0100

    Updated the way app manages routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_weather/screens/home.dart';
+import 'package:mobile_weather/screens/forecast_view.dart';
+import 'package:mobile_weather/screens/search.dart';
 
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Home(),
-));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    initialRoute: '/search',
+    routes: {
+      '/': (context) => const ForecastView(),
+      '/search': (context) => const Search(),
+    },
+  ));
+}
