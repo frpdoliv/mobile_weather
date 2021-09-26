2b89fe998d52bb0db494fe0050cea8c3fb7d5258
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-26 23:39:40 +0100

    MainScreenFrame was renamed to ForecastView and added an app bar

--- lib/screens/forecast_view.dart
+++ lib/screens/forecast_view.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_weather/providers/weather_data_fetcher.dart';
 import 'package:mobile_weather/screens/current_forecast.dart';
 import 'package:mobile_weather/screens/daily_forecast.dart';
 import 'package:mobile_weather/screens/hourly_forecast.dart';
-import 'package:mobile_weather/widgets/weather_list.dart';
 
-class MainScreenFrame extends StatefulWidget {
-  const MainScreenFrame({ Key? key }) : super(key: key);
+class ForecastView extends StatefulWidget {
+  const ForecastView({ Key? key }) : super(key: key);
 
   @override
-  State<MainScreenFrame> createState() => _MainScreenFrameState();
+  State<ForecastView> createState() => _ForecastViewState();
 }
 
-class _MainScreenFrameState extends State<MainScreenFrame> {
+class _ForecastViewState extends State<ForecastView> {
   int _selectedIndex = 1;
   final List<Widget> _screens = <Widget>[
     HourlyForecast(),
     CurrentForecast(),
     DailyForecast(),
   ];
-
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text('Leiria, Portugal'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.settings),
+          ),
+        ],
+        leading: IconButton(
+          onPressed: () => Navigator.pushNamed(context, '/search'),
+          icon: Icon(Icons.search),
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) => setState(() => _selectedIndex = index),
         currentIndex: _selectedIndex,
@@ -49,7 +60,7 @@ class _MainScreenFrameState extends State<MainScreenFrame> {
           ),
         ],
       ),
-      body: _screens[_selectedIndex]
+      body: _screens[_selectedIndex],
     );
   }
 }
