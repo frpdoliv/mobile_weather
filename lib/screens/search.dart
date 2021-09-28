61dea0b7bdd6d0d69de475bcb996359e1abca7b1
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-28 12:55:43 +0100

    Created the part that will display all the location cards

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_weather/model/location.dart';
 import 'package:mobile_weather/providers/open_weather_api_location_fetcher.dart';
+import 'package:mobile_weather/widgets/location_card.dart';
 import 'package:mobile_weather/widgets/location_search_bar.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Search extends StatefulWidget {
   const Search({ Key? key }) : super(key: key);
@@ -11,10 +13,17 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
-  List<Location>? _locationList;
+  Future<List<Location>>? _locationList;
 
-  void onUserSumission(List<String> userInput) async {
-    OpenWeatherAPILocationFetcher().fetchLocations(userInput);
+  void onUserSumission(List<String> userInput) {
+    //TODO Review this code
+    /*
+      _locationList = Future<List<Location>>.delayed(Duration(seconds: 2), () => OpenWeatherAPILocationFetcher().fetchLocations(userInput));
+      setState(() => print('NewData'));
+    */
+    setState(() {
+      _locationList = OpenWeatherAPILocationFetcher().fetchLocations(userInput);
+    });
   }
 
   @override
@@ -27,10 +36,42 @@ class _SearchState extends State<Search> {
               padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0),
               child: LocationAppBar(onSubmitted: onUserSumission),
             ),
+            Flexible(
+              child: FutureBuilder<List<Location>>(
+                future: _locationList,
+                initialData: const [],
+                builder: (BuildContext context, AsyncSnapshot<List<Location>> snapshot) {
+                  switch(snapshot.connectionState) {
+                    case ConnectionState.none:
+                      return Text('Try using the magnifiying glass to search for a location');
+                    case ConnectionState.active:
+                    case ConnectionState.waiting:
+                      return SpinKitCircle(color: Colors.red);
+                    case ConnectionState.done:
+                      List<Location> fetchedLocations = snapshot.data!;
+                      if (fetchedLocations.isEmpty){
+                        return const Text('No locations found');
+                      }
+                      else {
+                        return ListView.builder(
+                          itemCount: fetchedLocations.length,
+                          itemBuilder: (context, index) {
+                            return LocationCard(
+                              location: fetchedLocations[index],
+                              onTap: () {
+                                Navigator.pop(context, fetchedLocations[index]);
+                              },
+                            );
+                          },
+                        );
+                      }
+                  }
+                },
+              ),
+            ),
           ]
         ),
       ),
-      
     );
   }
 }
