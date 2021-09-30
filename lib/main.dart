a7a1941fa0fdbe72ed24a24f71d438a5b03c7a2b
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-30 21:38:32 +0100

    Added initial loading screen to the routing system

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_weather/screens/forecast_view.dart';
+import 'package:mobile_weather/screens/initial_loading.dart';
 import 'package:mobile_weather/screens/search.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
-    initialRoute: '/',
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/loading',
     routes: {
       '/': (context) => const ForecastView(),
       '/search': (context) => const Search(),
+      '/loading': (context) => const InitialLoading()
     },
   ));
 }
