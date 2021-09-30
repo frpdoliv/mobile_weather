bdbc827de8ae7efbe6c31cdcc7603659bd608c76
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-30 22:22:01 +0100

    Updated search to deal with errors

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_weather/model/io_failure.dart';
 import 'package:mobile_weather/model/location.dart';
-import 'package:mobile_weather/providers/open_weather_api_location_fetcher.dart';
+import 'package:mobile_weather/providers/open_weather_api_gps_location_fetcher.dart';
+import 'package:mobile_weather/providers/open_weather_api_user_input_location_fetcher.dart';
+import 'package:mobile_weather/widgets/blank_loading_screen.dart';
 import 'package:mobile_weather/widgets/location_card.dart';
 import 'package:mobile_weather/widgets/location_search_bar.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -16,13 +19,8 @@ class _SearchState extends State<Search> {
   Future<List<Location>>? _locationList;
 
   void onUserSumission(List<String> userInput) {
-    //TODO Review this code
-    /*
-      _locationList = Future<List<Location>>.delayed(Duration(seconds: 2), () => OpenWeatherAPILocationFetcher().fetchLocations(userInput));
-      setState(() => print('NewData'));
-    */
     setState(() {
-      _locationList = OpenWeatherAPILocationFetcher().fetchLocations(userInput);
+      _locationList = OpenWeatherAPIUserInputLocationFetcher(userInput).fetch();
     });
   }
 
@@ -61,30 +59,46 @@ class _SearchState extends State<Search> {
                 future: _locationList,
                 initialData: const [],
                 builder: (BuildContext context, AsyncSnapshot<List<Location>> snapshot) {
-                  switch(snapshot.connectionState) {
-                    case ConnectionState.none:
-                      return Text('Try using the magnifiying glass to search for a location');
-                    case ConnectionState.active:
-                    case ConnectionState.waiting:
-                      return SpinKitCircle(color: Colors.red);
-                    case ConnectionState.done:
-                      List<Location> fetchedLocations = snapshot.data!;
-                      if (fetchedLocations.isEmpty){
-                        return const Text('No locations found');
-                      }
-                      else {
-                        return ListView.builder(
-                          itemCount: fetchedLocations.length,
-                          itemBuilder: (context, index) {
-                            return LocationCard(
-                              location: fetchedLocations[index],
-                              onTap: () {
-                                Navigator.pop(context, fetchedLocations[index]);
-                              },
-                            );
-                          },
+                  if (snapshot.hasError) {
+                    if (snapshot.error is IOFailure) {
+                      WidgetsBinding.instance?.addPostFrameCallback((_) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("No internet connection"),
+                            backgroundColor: Colors.red,
+                          ),
                         );
-                      }
+                      });
+                      return const Text('Try using the magnifiying glass to search for a location');
+                    } else {
+                      throw snapshot.error!;
+                    }
+                  } else {
+                    switch(snapshot.connectionState) {
+                      case ConnectionState.none:
+                        return const Text('Try using the magnifiying glass to search for a location');
+                      case ConnectionState.active:
+                      case ConnectionState.waiting:
+                        return const BlankLoadingScreen();
+                      case ConnectionState.done:
+                        List<Location> fetchedLocations = snapshot.data!;
+                        if (fetchedLocations.isEmpty){
+                          return const Text('No locations found');
+                        }
+                        else {
+                          return ListView.builder(
+                            itemCount: fetchedLocations.length,
+                            itemBuilder: (context, index) {
+                              return LocationCard(
+                                location: fetchedLocations[index],
+                                onTap: () {
+                                  Navigator.pop(context, fetchedLocations[index]);
+                                },
+                              );
+                            },
+                          );
+                        }
+                    }
                   }
                 },
               ),
