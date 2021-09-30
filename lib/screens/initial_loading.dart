ab1abf43c2d0820529d5fb58cf53bf136650334c
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-30 21:37:18 +0100

    Implemented initial loading screen

--- lib/screens/initial_loading.dart
+++ lib/screens/initial_loading.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:mobile_weather/model/io_failure.dart';
 import 'package:mobile_weather/model/location.dart';
-import 'package:mobile_weather/model/location_forecast.dart';
-import 'package:mobile_weather/providers/open_weather_api_forecast_fetcher.dart';
 import 'package:mobile_weather/providers/open_weather_api_gps_location_fetcher.dart';
 import 'package:mobile_weather/providers/open_weather_api_user_input_location_fetcher.dart';
 
@@ -14,25 +13,55 @@ class InitialLoading extends StatefulWidget {
 }
 
 class _InitialLoadingState extends State<InitialLoading> {
-  Future<void> _loadDefaultLocation() async {
-    int initMilliseconds = DateTime.now().millisecondsSinceEpoch;
-    Location? location = await OpenWeatherAPIGPSLocationFetcher().fetch();
-    if (location == null) {
-      List<Location> inputLocations = await OpenWeatherAPIUserInputLocationFetcher(['Lisbon', 'PT']).fetch();
-      location = inputLocations[0];
-    }
+  bool _isSnackbarActive = false;
 
-    OpenWeatherAPIForecastFetcher forecastFetcher = await OpenWeatherAPIForecastFetcher.create(longitude: location.longitude, latitude: location.latitude);
-    int remainingTime = 1000 - (DateTime.now().millisecondsSinceEpoch - initMilliseconds);
-    Future.delayed(
-      Duration(milliseconds: remainingTime),
-      () {
-        Navigator.popAndPushNamed(context, '/', arguments: {
-          'location': location,
-          'forecast': forecastFetcher.locationForecast,
-        });
+  void _closeSnackBars(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    _isSnackbarActive = false;
+  }
+
+  void _addSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text('No internet connection'),
+            action: SnackBarAction(
+              label: 'Retry',
+              onPressed: () {
+                _closeSnackBars(context);
+                _loadDefaultLocation();
+              },
+            ),
+            duration: const Duration(
+              days: 10000,
+            ),
+          )
+    ).closed.then((_) => _isSnackbarActive = false);
+    _isSnackbarActive = true;
+  }
+
+  Future<void> _loadDefaultLocation() async {
+    try {
+      int initMilliseconds = DateTime.now().millisecondsSinceEpoch;
+      Location? location = await OpenWeatherAPIGPSLocationFetcher().fetch();
+      if (location == null) {
+        List<Location> inputLocations = await OpenWeatherAPIUserInputLocationFetcher(['Lisbon', 'PT']).fetch();
+        location = inputLocations[0];
       }
-    );
+
+      _closeSnackBars(context);
+      int remainingTime = 1000 - (DateTime.now().millisecondsSinceEpoch - initMilliseconds);
+      Future.delayed(
+        Duration(milliseconds: remainingTime),
+        () {
+          Navigator.popAndPushNamed(context, '/', arguments: location);
+        }
+      );
+    } on IOFailure {
+      if (!_isSnackbarActive) {
+        _addSnackBar(context);
+      }
+      Future.delayed(const Duration(seconds: 1, milliseconds: 500), _loadDefaultLocation);
+    }
   }
   
   @override
@@ -43,8 +72,8 @@ class _InitialLoadingState extends State<InitialLoading> {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
+    return const Scaffold(
+      body: Center(
         child: SpinKitCircle(color: Colors.red),
       ),
     );
