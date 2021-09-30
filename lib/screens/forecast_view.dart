8a55b0490479f8c574ed6b27b2abf1de5262b0e6
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-30 22:23:14 +0100

    Updated view to deal with new forecast view widgets

--- lib/screens/forecast_view.dart
+++ lib/screens/forecast_view.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_weather/model/location.dart';
+import 'package:mobile_weather/model/location_forecast.dart';
+import 'package:mobile_weather/providers/forecast_fetcher.dart';
+import 'package:mobile_weather/providers/open_weather_api_forecast_fetcher.dart';
 import 'package:mobile_weather/screens/current_forecast.dart';
 import 'package:mobile_weather/screens/daily_forecast.dart';
+import 'package:mobile_weather/screens/forecast_screen.dart';
 import 'package:mobile_weather/screens/hourly_forecast.dart';
+import 'package:mobile_weather/widgets/blank_loading_screen.dart';
 
 class ForecastView extends StatefulWidget {
   const ForecastView({ Key? key }) : super(key: key);
@@ -13,13 +18,20 @@ class ForecastView extends StatefulWidget {
 
 class _ForecastViewState extends State<ForecastView> {
   int _selectedIndex = 1;
-  late Location location;
-  final List<Widget> _screens = <Widget>[
-    HourlyForecast(),
-    CurrentForecast(),
-    DailyForecast(),
-  ];
-  
+  Location? _location;
+  ForecastFetcher _forecastFetcher = OpenWeatherAPIForecastFetcher();
+
+  Location? get location {
+    return _location;
+  }
+
+  set location(Location? location) {
+    _location = location;
+    if (location != null) {
+      _forecastFetcher = OpenWeatherAPIForecastFetcher.withCoordinates(longitude: location.longitude, latitude: location.latitude);
+    }
+  }
+
   void userChangeLocation() async {
     Location? searchResult = await Navigator.pushNamed(context, '/search') as Location?;
     setState(() {
@@ -27,49 +39,73 @@ class _ForecastViewState extends State<ForecastView> {
     });
   }
 
+  final List<ForecastScreen Function(LocationForecast locationForecast)> _screenBuilders = [
+    (locationForecast) => HourlyForecast(locationForecast: locationForecast),
+    (locationForecast) => CurrentForecast(locationForecast: locationForecast),
+    (locationForecast) => DailyForecast(locationForecast: locationForecast),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        title: Text('Leiria, Portugal'),
-        actions: <Widget>[
-          IconButton(
-            onPressed: (){},
-            icon: Icon(Icons.settings),
+    location ??= ModalRoute.of(context)?.settings.arguments as Location?;
+    if (location == null) {
+      return const Text("You're not supposed to be here");
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          title: Text(_location.toString()),
+          actions: <Widget>[
+            IconButton(
+              onPressed: (){},
+              icon: const Icon(Icons.settings),
+            ),
+          ],
+          leading: IconButton(
+            onPressed: userChangeLocation,
+            icon: const Icon(Icons.search),
           ),
-        ],
-        leading: IconButton(
-          onPressed: userChangeLocation,
-          icon: const Icon(Icons.search),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) => setState(() => _selectedIndex = index),
-        currentIndex: _selectedIndex,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.red,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.grey,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            activeIcon: Icon(Icons.schedule),
-            icon: Icon(Icons.schedule_outlined),
-            label: 'Hourly',
-          ),
-          BottomNavigationBarItem(
-            activeIcon: Icon(Icons.place),
-            icon: Icon(Icons.place_outlined),
-            label: 'Current',
-          ),
-          BottomNavigationBarItem(
-            activeIcon: Icon(Icons.calendar_today),
-            icon: Icon(Icons.calendar_today_outlined),
-            label: 'Daily',
-          ),
-        ],
-      ),
-      body: _screens[_selectedIndex],
-    );
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) => setState(() => _selectedIndex = index),
+          currentIndex: _selectedIndex,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.red,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              activeIcon: Icon(Icons.schedule),
+              icon: Icon(Icons.schedule_outlined),
+              label: 'Hourly',
+            ),
+            BottomNavigationBarItem(
+              activeIcon: Icon(Icons.place),
+              icon: Icon(Icons.place_outlined),
+              label: 'Current',
+            ),
+            BottomNavigationBarItem(
+              activeIcon: Icon(Icons.calendar_today),
+              icon: Icon(Icons.calendar_today_outlined),
+              label: 'Daily',
+            ),
+          ],
+        ),
+        body: FutureBuilder(
+          future: _forecastFetcher.locationForecast,
+          builder: (BuildContext context, AsyncSnapshot<LocationForecast> snapshot) {
+            switch(snapshot.connectionState) { 
+              case ConnectionState.none:
+                return const BlankLoadingScreen();
+              case ConnectionState.active:
+              case ConnectionState.waiting:
+                return const BlankLoadingScreen();
+              case ConnectionState.done:
+                return _screenBuilders[_selectedIndex](snapshot.data as LocationForecast);
+            }
+          },
+        ),
+      );
+    }
   }
 }
