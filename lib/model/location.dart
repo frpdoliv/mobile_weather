24367fd7c2f9473d97c27cbafad116b98e0dbd49
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-30 21:44:13 +0100

    Coordinates are now stored as doubles

--- lib/model/location.dart
+++ lib/model/location.dart
@@ -2,8 +2,8 @@ class Location {
   String countryCode;
   String defaultCityName;
   String? stateName;
-  String latitude;
-  String longitude;
+  double latitude;
+  double longitude;
   Map<String, String>? cityNames;
 
   Location({required this.countryCode, this.cityNames, required this.defaultCityName, this.stateName, required this.latitude, required this.longitude});
