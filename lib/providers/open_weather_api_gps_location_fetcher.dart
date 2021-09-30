8a59534708fd6697183024894b6001d6e340d27f
Francisco Oliveira <fr.pdoliv@gmail.com>
2021-09-30 22:19:52 +0100

    Updated latitude and longitude to be a double

--- lib/providers/open_weather_api_gps_location_fetcher.dart
+++ lib/providers/open_weather_api_gps_location_fetcher.dart
@@ -1,20 +1,18 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:mobile_weather/model/location.dart';
 import 'package:mobile_weather/providers/gps_location_fetcher.dart';
+import 'package:mobile_weather/providers/json_provider.dart';
 
 class OpenWeatherAPIGPSLocationFetcher extends GPSLocationFetcher {
 
   @override
   Future<Location> getAPIData({required double latitude, required double longitude}) async {
-    http.Response response = await http.get(Uri.parse("http://api.openweathermap.org/geo/1.0/reverse?lat=$latitude&lon=$longitude&limit=1&appid=$_apiKey"));
-    Map<String, dynamic> locationJSON = jsonDecode(response.body)[0];
-    return Location(
-      latitude: locationJSON['lat'].toString(),
-      longitude: locationJSON['lon'].toString(),
+    Map<String, dynamic> locationJSON = (await JSONProvider.get(Uri.parse("http://api.openweathermap.org/geo/1.0/reverse?lat=$latitude&lon=$longitude&limit=1&appid=$_apiKey")))[0];
+    return Location.withAttr(
+      latitude: double.parse(locationJSON['lat'].toString()),
+      longitude: double.parse(locationJSON['lon'].toString()),
       defaultCityName: locationJSON['name'].toString(),
       countryCode: locationJSON['country'].toString(),
-      stateName: locationJSON['state'].toString(),
+      stateName: locationJSON['state'],
       cityNames: locationJSON['local_names'] != null ? Map<String, String>.from(locationJSON['local_names']) : null,
     );
   }
--- lib/providers/open_weather_api_user_input_location_fetcher.dart
+++ lib/providers/open_weather_api_user_input_location_fetcher.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:mobile_weather/model/location.dart';
+import 'package:mobile_weather/providers/json_provider.dart';
 import 'package:mobile_weather/providers/user_input_location_fetcher.dart';
 
 class OpenWeatherAPIUserInputLocationFetcher implements UserInputLocationFetcher {
@@ -11,12 +12,11 @@ class OpenWeatherAPIUserInputLocationFetcher implements UserInputLocationFetcher
 
   @override
   Future<List<Location>> fetch() async {
-    http.Response response = await http.get(Uri.parse('http://api.openweathermap.org/geo/1.0/direct?q=${locationStrings.join(',')}&limit=5&appid=$_apiKey'));
-    List<dynamic> locationJSON = jsonDecode(response.body);
+    List<dynamic> locationJSON = await JSONProvider.get(Uri.parse('http://api.openweathermap.org/geo/1.0/direct?q=${locationStrings.join(',')}&limit=5&appid=$_apiKey'));
     return locationJSON.map<Location>((location) {
-      return Location(
-        latitude: location['lat'].toString(),
-        longitude: location['lon'].toString(),
+      return Location.withAttr(
+        latitude: double.parse(location['lat'].toString()),
+        longitude: double.parse(location['lon'].toString()),
         stateName: location['state'],
         countryCode: location['country'],
         defaultCityName: location['name'],
